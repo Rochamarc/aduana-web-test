60fcdcb7f91c903efba375e34d67d64a7ced0a26
nuke <rochadcmarcos@gmail.com>
2021-05-24 17:03:40 -0300

    test: sessions
    created sessions tests

---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe 'Sesisons API', type: :request do 
+    let(:user) { create(:user) }
+    let(:headers) do 
+        {
+            'Content-Type' => Mime[:json].to_s 
+        }
+    end
+
+    describe 'POST /sessions' do 
+        before do
+            post '/sessions', params: { session: credentials }.to_json, headers: headers
+        end
+
+        context 'when the credentials are correct' do 
+            let(:credentials) { { email: user.email, password: '123456' }}
+
+            it 'returns status code 200' do 
+                expect(response).to have_http_status(200)
+            end
+
+            it 'returns the json data for the user with auth token' do 
+                json_body = JSON.parse(response.body)
+                user.reload
+                expect(json_body['auth_token']).to eq(user.auth_token)
+            end
+        end
+
+        context 'when the credentials are incorrect' do 
+            let(:credentials) { { email: user.email, password: '654321' }}
+
+            it 'returns status code 401' do 
+                expect(response).to have_http_status(401)
+            end
+
+            it 'returns the json data for the errors' do 
+                json_body = JSON.parse(response.body)
+                expect(json_body).to have_key('errors')
+            end
+        end
+    end
+
+    describe 'DELETE /sessions/:id' do 
+        let(:auth_token) { user.auth_token }
+
+        before do 
+            delete "/sessions/#{auth_token}", params: {}, headers: headers
+        end
+
+        it 'returns status code 204' do 
+            expect(response).to have_http_status(204)
+        end
+        
+        it 'changes user auth token' do 
+            expect( User.find_by(auth_token: auth_token)).to be_nil
+        end
+    end
+end
