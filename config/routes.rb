9569575ef699e46845cfbc4631d4dd82ce295ce2
nuke <rochadcmarcos@gmail.com>
2021-05-24 16:44:57 -0300

    feat: sessions created
    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users, only: [:sessions], controllers: { sessions: 'sessions' }
 
   resources :users, except: :index
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
